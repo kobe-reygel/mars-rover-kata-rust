6a1a8242650a287ce71dade524c7275072a1ea80
kobe63 <kobe63@users.noreply.github.com>
2023-11-09 00:50:30 +0100

    req1: starting point and direction - succesful implementation

--- src/rust_rover/rust_rover.rs
+++ src/rust_rover/rust_rover.rs
@@ -1,15 +1,40 @@
+#[derive(PartialEq, Debug, Clone)]
+struct RustRover {
+    coordinate: Coordinate,
+    direction: Direction,
+}
 
+impl RustRover {
+    fn new(coordinate: Coordinate, direction: Direction) -> RustRover {
+        RustRover {
+            coordinate,
+            direction
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+struct Coordinate {
+    x: i32,
+    y: i32,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+enum Direction {
+    North,
+}
 
 
 
 #[cfg(test)]
 mod rust_rover_tests {
+    use crate::rust_rover::rust_rover::*;
 
     #[test]
     fn rover_can_be_initialized_with_starting_point_and_direction() {
         let starting_coordinate: Coordinate = Coordinate { x: 0, y: 0 };
         let starting_direction: Direction = Direction::North;
-        let rover: RustRover = RustRover::new(starting_coordinate, starting_direction);
+        let rover: RustRover = RustRover::new(starting_coordinate.clone(), starting_direction.clone());
         assert_eq!(rover.coordinate, starting_coordinate);
         assert_eq!(rover.direction, starting_direction);
     }
