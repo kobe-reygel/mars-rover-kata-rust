8b06d4c812b8dadb901d2f82a522b2ac4e9a6126
kobe63 <kobe63@users.noreply.github.com>
2023-11-11 00:32:21 +0100

    req3.2: backward move - test east red

--- src/domain/rust_rover_aggregate.rs
+++ src/domain/rust_rover_aggregate.rs
@@ -141,5 +141,12 @@ mod rust_rover_tests {
             rover.move_backward();
             assert_eq!(rover.coordinate, Coordinate::from(0, -1));
         }
+
+        #[test]
+        fn move_backward_when_rover_facing_east_then_moves_left() {
+            let mut rover: RustRover = RustRover::new(Coordinate::from(0, 0), Direction::East);
+            rover.move_backward();
+            assert_eq!(rover.coordinate, Coordinate::from(-1, 0));
+        }
     }
 }
