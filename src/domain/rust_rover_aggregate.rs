3b007232ee49f4889a2b8e8b0e59aa3e52fa04f6
kobe63 <kobe63@users.noreply.github.com>
2023-11-11 00:11:11 +0100

    req3.1: forward move - test north

--- src/domain/rust_rover_aggregate.rs
+++ src/domain/rust_rover_aggregate.rs
@@ -53,40 +53,52 @@ pub enum Direction {
 
 #[cfg(test)]
 mod rust_rover_tests {
-    use crate::domain::rust_rover_aggregate::*;
+    mod initialization_tests {
+        use crate::domain::rust_rover_aggregate::*;
 
-    #[test]
-    fn rover_can_be_initialized_with_starting_point_and_direction() {
-        let rover: RustRover = RustRover::new(Coordinate::from(14,0), Direction::North);
-        assert_eq!(rover.coordinate, Coordinate::from(14,0));
-        assert_eq!(rover.direction, Direction::North);
+        #[test]
+        fn rover_can_be_initialized_with_starting_point_and_direction() {
+            let rover: RustRover = RustRover::new(Coordinate::from(14,0), Direction::North);
+            assert_eq!(rover.coordinate, Coordinate::from(14,0));
+            assert_eq!(rover.direction, Direction::North);
+        }
+
+        #[test]
+        fn rover_can_have_north_direction() {
+            let rover: RustRover = construct_rover_with_direction(Direction::North);
+            assert_eq!(rover.direction, Direction::North);
+        }
+
+        #[test]
+        fn rover_can_have_east_direction() {
+            let rover: RustRover = construct_rover_with_direction(Direction::East);
+            assert_eq!(rover.direction, Direction::East);
+        }
+
+        #[test]
+        fn rover_can_have_south_direction() {
+            let rover: RustRover = construct_rover_with_direction(Direction::South);
+            assert_eq!(rover.direction, Direction::South);
+        }
+
+        #[test]
+        fn rover_can_have_west_direction() {
+            let rover: RustRover = construct_rover_with_direction(Direction::West);
+            assert_eq!(rover.direction, Direction::West);
+        }
+
+        fn construct_rover_with_direction(direction: Direction) -> RustRover {
+            RustRover::new(Coordinate::from(0,0), direction)
+        }
     }
 
-    #[test]
-    fn rover_can_have_north_direction() {
-        let rover: RustRover = construct_rover_with_direction(Direction::North);
-        assert_eq!(rover.direction, Direction::North);
-    }
-
-    #[test]
-    fn rover_can_have_east_direction() {
-        let rover: RustRover = construct_rover_with_direction(Direction::East);
-        assert_eq!(rover.direction, Direction::East);
-    }
-
-    #[test]
-    fn rover_can_have_south_direction() {
-        let rover: RustRover = construct_rover_with_direction(Direction::South);
-        assert_eq!(rover.direction, Direction::South);
-    }
-
-    #[test]
-    fn rover_can_have_west_direction() {
-        let rover: RustRover = construct_rover_with_direction(Direction::West);
-        assert_eq!(rover.direction, Direction::West);
-    }
-
-    fn construct_rover_with_direction(direction: Direction) -> RustRover {
-        RustRover::new(Coordinate::from(0,0), direction)
+    mod movement_tests {
+        use crate::domain::rust_rover_aggregate::*;
+        #[test]
+        fn move_forward_when_rover_facing_north_then_moves_up() {
+            let mut rover: RustRover = RustRover::new(Coordinate::from(0,0), Direction::North);
+            rover.move_forward();
+            assert_eq!(rover.coordinate, Coordinate::from(0,1));
+        }
     }
 }
